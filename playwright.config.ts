54c52ffcf9c6b97f78e7900f1cea552fb4bbda8a
build-vtest <build@vtestcorp.com>
2024-01-30 10:57:38 +0530

    code changes playwright file

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,9 +24,9 @@ export default defineConfig({
   /* Retry on CI only */
   // retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: 2,
+  workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['html', { open: 'never', printSteps: true }], ['allure-playwright']],
+  reporter: [['html', { open: 'never', printSteps: true }], ['allure-playwright'],['junit', {outputFile:'junit-report/junit-report.xml'}]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
