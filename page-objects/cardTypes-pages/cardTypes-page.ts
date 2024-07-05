a15a1ff718389064c3d85586467b87347bf0b16f
build-vtest <build@vtestcorp.com>
2024-07-05 18:54:14 +0530

    code of 5-7-2024

--- page-objects/cardTypes-pages/cardTypes-page.ts
+++ page-objects/cardTypes-pages/cardTypes-page.ts
@@ -10,7 +10,11 @@ export class CardTypesPage {
     readonly organization: Locator;
     readonly organizationName: Locator;
     readonly organizationCode: Locator;
-
+    readonly search: Locator;
+    readonly financialsInformation: Locator;
+    readonly orgCardType: Locator;
+    readonly breadcrumbsCardType: Locator;
+    readonly addOrgCardType: Locator;
 
 
 
@@ -22,9 +26,11 @@ export class CardTypesPage {
         this.organization = page.locator('//h1[@title="Organizations"]');
         this.organizationName = page.locator('//div//input[@title="Name"]');
         this.organizationCode = page.locator('//div//input[@title="Code"]');
-
-
-
+        this.search = page.locator('//div//button[@title="Search"]');
+        this.financialsInformation = page.locator('//iris-navigation-hyperlink//a[@title="Financial Information"]');
+        this.orgCardType = page.locator('//iris-navigation-hyperlink//a[@title="Card Types"]');
+        this.breadcrumbsCardType = page.locator('//iris-breadcrumb//li//iris-navigation-hyperlink//a[@href="/organization/details/card-types"]');
+        this.addOrgCardType = page.locator('//button[@title="Add Organization Card Type"]');
     }
 
     async verifyBreadCrumbsText(data: string) {
@@ -60,7 +66,74 @@ export class CardTypesPage {
         }
     }
 
+    async enterOrganizationCode(data: string) {
+        await this.organizationCode.fill(data);
+    }
+
+    async enterOrganizationName(data: string) {
+        await new Promise(resolve => setTimeout(resolve, 3000));
+        await this.organizationName.fill(data);
+    }
+
+    async clickOnSearch() {
+        await this.search.click();
+        await this.page.waitForSelector('.loader', { state: 'hidden' });
+    }
+
+    async verifyOrgNameAndCodeFromGrid() {
+        await new Promise(resolve => setTimeout(resolve, 5000));
+        const Name = this.page.locator('//mat-cell[contains(@class,"name")]');
+        for (let index = 0; index < await Name.count(); index++) {
+            expect(await Name.nth(index).innerText()).toBeTruthy();
+
+        }
+        const code = this.page.locator('//mat-cell[contains(@class,"code")]');
+        for (let index = 0; index < await code.count(); index++) {
+            expect(await code.nth(index).innerText()).toBeTruthy();
+
+        }
+    }
+
+    async VerifyEditOrganizationsButtons() {
+        await new Promise(resolve => setTimeout(resolve, 3000));
+        const editOrgIcon = this.page.locator('//button[@title="Edit Organization"]');
+        for (let index = 0; index < await editOrgIcon.count(); index++) {
+            expect(editOrgIcon.nth(index)).toBeVisible();
+        }
+    }
+
+    async clickonEditOrganizationButton() {
+        await new Promise(resolve => setTimeout(resolve, 3000));
+        const editOrgIcon = this.page.locator('//button[@title="Edit Organization"]');
+        await editOrgIcon.first().click();
+    }
+
+
+    async verifyFinancialInfo(data: string) {
+        expect(this.financialsInformation).toBeVisible();
+    }
+
+    async verifyOrgCardType(data: string) {
+        expect(this.orgCardType).toBeVisible();
+    }
+
+    async clickonCardType() {
+        await new Promise(resolve => setTimeout(resolve, 3000));
+        await this.orgCardType.click();
+    }
+
+    async verifyBreadcrumbsCardType(data: string) {
+        const actual = await this.breadcrumbsCardType.textContent();
+        expect(actual).toBe(data);
+    }
+
+    async clickonAddCardType() {
+        await new Promise(resolve => setTimeout(resolve, 3000));
+        await this.addOrgCardType.click();
+    }
 
 
 }
+
+
 
--- tests/cardTypes/cardTypes.spec.ts
+++ tests/cardTypes/cardTypes.spec.ts
@@ -68,3 +68,181 @@ test('NXGF-TC-1857: TC_001_card types_Verify that the user is able to view the a
 });
 
 
+test('NXGF-TC-1858: TC_002_card types_Verify that the user is able to search for any organization with organization name or code', async ({ page }) => {
+
+    const loginPage: LoginPage = new LoginPage(page);
+    const dashboardPage: DashboardPage = new DashboardPage(page);
+    const cardTypesPage: CardTypesPage = new CardTypesPage(page);
+
+    await test.step("User navigates to Mawista application", async () => {
+        await loginPage.gotoLoginPage(data['Login-Access'].url);
+    })
+
+    await test.step("User Enter Username and Password", async () => {
+        await loginPage.loginToApplication(data['Login-Access'].userNameInput, data['Login-Access'].passwordInput);
+    })
+
+    await test.step("User Click on Menu Icon", async () => {
+        await dashboardPage.clickOnMenuIconButton();
+    })
+
+    await test.step("Click on the Organization shortcut icon", async () => {
+        await cardTypesPage.clickOnFinancialOrganizationShrtcutsButton();
+    })
+
+    await test.step("User Enter Organization Name", async () => {
+        await cardTypesPage.enterOrganizationName(CardTypesData['TC_002_card types'].organizationName);
+    })
+
+    await test.step("User Enter Organization Code", async () => {
+        await cardTypesPage.enterOrganizationCode(CardTypesData['TC_002_card types'].organizationCode);
+    })
+
+    await test.step("User Click on Search", async () => {
+        await cardTypesPage.clickOnSearch();
+    })
+
+    await test.step("Verified that the user is able to search for any organization with organization name or code", async () => {
+        await cardTypesPage.verifyOrgNameAndCodeFromGrid();
+    })
+
+});
+
+test('NXGF-TC-1859: TC_003_card types_Verify that the user is able to view the details option in the search result grid', async ({ page }) => {
+
+    const loginPage: LoginPage = new LoginPage(page);
+    const dashboardPage: DashboardPage = new DashboardPage(page);
+    const cardTypesPage: CardTypesPage = new CardTypesPage(page);
+
+    await test.step("User navigates to Mawista application", async () => {
+        await loginPage.gotoLoginPage(data['Login-Access'].url);
+    })
+
+    await test.step("User Enter Username and Password", async () => {
+        await loginPage.loginToApplication(data['Login-Access'].userNameInput, data['Login-Access'].passwordInput);
+    })
+
+    await test.step("User Click on Menu Icon", async () => {
+        await dashboardPage.clickOnMenuIconButton();
+    })
+
+    await test.step("Click on the Organization shortcut icon", async () => {
+        await cardTypesPage.clickOnFinancialOrganizationShrtcutsButton();
+    })
+
+    await test.step("Verified that the Edit Organization Button is displayed", async () => {
+        await cardTypesPage.VerifyEditOrganizationsButtons();
+    })
+
+});
+
+test('NXGF-TC-1860: TC_004_card types_Verify that the user is able to view two options Financial information and Organization card types', async ({ page }) => {
+
+    const loginPage: LoginPage = new LoginPage(page);
+    const dashboardPage: DashboardPage = new DashboardPage(page);
+    const cardTypesPage: CardTypesPage = new CardTypesPage(page);
+
+    await test.step("User navigates to Mawista application", async () => {
+        await loginPage.gotoLoginPage(data['Login-Access'].url);
+    })
+
+    await test.step("User Enter Username and Password", async () => {
+        await loginPage.loginToApplication(data['Login-Access'].userNameInput, data['Login-Access'].passwordInput);
+    })
+
+    await test.step("User Click on Menu Icon", async () => {
+        await dashboardPage.clickOnMenuIconButton();
+    })
+
+    await test.step("Click on the Organization shortcut icon", async () => {
+        await cardTypesPage.clickOnFinancialOrganizationShrtcutsButton();
+    })
+
+    await test.step("Click on Edit Organization button", async () => {
+        await cardTypesPage.clickonEditOrganizationButton();
+    })
+
+    await test.step("Verified that the after click on details button Financials information and Organization card type is displayed", async () => {
+        await cardTypesPage.verifyFinancialInfo(CardTypesData['TC_004_card types'].financialInfo);
+    })
+
+    await test.step("Verified that the after click on details button Financials information and Organization card type is displayed", async () => {
+        await cardTypesPage.verifyOrgCardType(CardTypesData['TC_004_card types'].cardTypes);
+    })
+
+});
+
+test('NXGF-TC-1861: TC_005_card types_Verify that the user is able to click on the Organization Card types and is redirected to Organization Card Types screen', async ({ page }) => {
+
+    const loginPage: LoginPage = new LoginPage(page);
+    const dashboardPage: DashboardPage = new DashboardPage(page);
+    const cardTypesPage: CardTypesPage = new CardTypesPage(page);
+
+    await test.step("User navigates to Mawista application", async () => {
+        await loginPage.gotoLoginPage(data['Login-Access'].url);
+    })
+
+    await test.step("User Enter Username and Password", async () => {
+        await loginPage.loginToApplication(data['Login-Access'].userNameInput, data['Login-Access'].passwordInput);
+    })
+
+    await test.step("User Click on Menu Icon", async () => {
+        await dashboardPage.clickOnMenuIconButton();
+    })
+
+    await test.step("Click on the Organization shortcut icon", async () => {
+        await cardTypesPage.clickOnFinancialOrganizationShrtcutsButton();
+    })
+
+    await test.step("Click on Edit Organization button", async () => {
+        await cardTypesPage.clickonEditOrganizationButton();
+    })
+
+    await test.step("Click on Card Types", async () => {
+        await cardTypesPage.clickonCardType();
+    })
+
+    await test.step("Verified that the user is able to click on the Organization Card types and is redirected to Organization Card Types screen", async () => {
+        await cardTypesPage.verifyBreadcrumbsCardType(CardTypesData['TC_005_card types'].breadcrumbsCardType);
+    })
+
+});
+
+test('NXGF-TC-1862: TC_006_card types_Verify that the user is able to add a new card with add(+) option', async ({ page }) => {
+
+    const loginPage: LoginPage = new LoginPage(page);
+    const dashboardPage: DashboardPage = new DashboardPage(page);
+    const cardTypesPage: CardTypesPage = new CardTypesPage(page);
+
+    await test.step("User navigates to Mawista application", async () => {
+        await loginPage.gotoLoginPage(data['Login-Access'].url);
+    })
+
+    await test.step("User Enter Username and Password", async () => {
+        await loginPage.loginToApplication(data['Login-Access'].userNameInput, data['Login-Access'].passwordInput);
+    })
+
+    await test.step("User Click on Menu Icon", async () => {
+        await dashboardPage.clickOnMenuIconButton();
+    })
+
+    await test.step("Click on the Organization shortcut icon", async () => {
+        await cardTypesPage.clickOnFinancialOrganizationShrtcutsButton();
+    })
+
+    await test.step("Click on Edit Organization button", async () => {
+        await cardTypesPage.clickonEditOrganizationButton();
+    })
+
+    await test.step("Click on Card Types", async () => {
+        await cardTypesPage.clickonCardType();
+    })
+
+    await test.step("Verified that the user is able to add a new card with add(+) option", async () => {
+        await cardTypesPage.clickonAddCardType();
+    })
+
+});
+
+
+
