e2736e547164d8a27c97b50aad800625f97ad66d
Sunil Yeskar <ashwini.sunil.yeskar@accenture.com>
2026-07-13 13:08:41 +0530

    code commit on 13th july 2026

--- page-objects/login-page.ts
+++ page-objects/login-page.ts
@@ -13,11 +13,17 @@ export class LoginPage {
 
     constructor(page: Page) {
         this.page = page;
-        this.userNameOrEmailInputField = page.locator("//input[@id='mat-input-0']");
-        this.passwordInputField = page.locator("//input[@id='mat-input-1']");
+        // this.userNameOrEmailInputField = page.locator("//input[@id='mat-input-0']");
+        // this.passwordInputField = page.locator("//input[@id='mat-input-1']");
         //this.signinButton = page.locator('span[title="Sign in"]');
        // code updated 19-12-2024
-       this.signinButton = page.locator("//button[@type='submit']");
+       //this.signinButton = page.locator("//button[@type='submit']");
+       
+        this.userNameOrEmailInputField = page.locator("//mat-label[contains(normalize-space(.), 'Email or Username')]/ancestor::mat-form-field//input");
+        this.passwordInputField = page.locator("//mat-label[contains(normalize-space(.), 'Password')]/ancestor::mat-form-field//input");
+        //this.signinButton = page.locator('span[title="Sign in"]');
+       // code updated 19-12-2024
+        this.signinButton = page.locator("//button[@type='submit']");
         this.rememberMeCheckbox = page.locator('//mat-checkbox');
         this.checkBoxSelector= page.locator("//input[contains(@type,'checkbox')]");
         this.captchaError= page.locator("//mat-icon[@title='Cancel']//*[name()='svg']");
